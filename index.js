cfd658dac2834a1ec600f41a7bfb7481c72859a2
devdan <oluochdancun19@gmail.com>
2022-06-04 22:44:46 +0300

    js loops

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names =["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names){
+    for(let x=0; x < names.length; x++){
+      console.log( `"Thank you, ${names[x]}, for the wonderful surprise gift!",`.split(","));
+              
+    }
+
+}
+writeCards(names)
+
+
+function countDown(number){
+     while(number>=0){
+         number--;
+         console.log(number);
+
+     }
+ }
+ countDown(10)
